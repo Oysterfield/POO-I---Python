8cb77d7e553633b15144032ea2abc72668db9ccc
Oysterfield <116518676+Oysterfield@users.noreply.github.com>
2024-09-24 14:21:36 -0300

    Add files via upload

---
+++ Classe Banco.py
@@ -0,0 +1,24 @@
+class Banco:
+
+    def __init__(self):
+        self.nome = nome
+        self.cnpj = cnpj
+        self.ultima_conta_criada = ultima_conta_criada
+        #self.fichario = fichario
+
+    def abre_conta(ultima_conta_criada, nome, cpf):
+        ultima_conta_criada +=1
+        return num_conta
+    
+    def deposito(self,num_conta, saldo, valor):
+        return self.saldo
+    
+    def saque(self,num_conta, saldo, valor):
+        return self.saldo
+    
+    
+    def transferencia(self,num_conta_orig, num_conta_destino, valor):
+        return self.num_conta_orig
+    
+    def encerra_conta(num_conta):
+        del num_conta
